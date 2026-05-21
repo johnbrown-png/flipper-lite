49478232a80b55c5b6332d55efcefe3ed7effb0a
John Brown <john.brown@flipper.school>
2026-05-21 15:47:11 +0100

    age

--- search_app/curriculum_assistant.py
+++ search_app/curriculum_assistant.py
@@ -84,7 +84,7 @@ class CurriculumAssistant:
         }
         </style>
         """, unsafe_allow_html=True)
-        age_col, _age_spacer_col = st.columns([1, 14])
+        age_col, _age_spacer_col = st.columns([4, 11])
         with age_col:
             selected_year = st.selectbox(
                 "Age",
